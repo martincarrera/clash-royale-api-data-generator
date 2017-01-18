995482c88f78e93c38126daaefc7d95c9bba6d79
Martin Carrera <mcarrera.92@gmail.com>
2017-01-18 17:30:31 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,36 @@
+const request = require('request');
+const arenas = require('./data/arenas.json');
+const cards = require('./data/cards.json');
+const players = require('./data/players.json');
+//const chests = require('./data/chests.json');
+const user = { username: 'admin', password: 'admin' };
+const uri = 'http://localhost:8085/';
+
+request.post({
+    url: uri + 'api/authenticate',
+    headers: {
+      'Accept': 'application/json'
+    },
+    form: user
+  }, (err, httpResponse, body) => {
+    const token = body;
+    save(token, 'api/cards', cards);
+    //save(token, 'api/chests', chests);
+    save(token, 'api/players', players);
+    save(token, 'api/arenas', arenas);
+});
+
+const save = (token, endpoint, objects) => {
+  setInterval(objects.forEach((o) => {
+    request.post({
+        url: uri + endpoint,
+        headers: {
+          'Authorization': token,
+          'Accept': 'application/json'
+        },
+        form: o
+      }, (err, httpResponse, body) => {
+        console.log(o);
+      });
+  }), 1000);
+};

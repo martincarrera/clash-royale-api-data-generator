f28609b59cee8c95b0504b113081c8bd81e41109
Martin Carrera <mcarrera.92@gmail.com>
2017-02-05 13:44:51 -0300

    Added .env to gitignore and dotenv dependency

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+require('dotenv').load();
 const request = require('request');
 const arenas = require('./data/arenas.json');
 const cards = require('./data/cards.json');
 const players = require('./data/players.json');
-//const chests = require('./data/chests.json');
-const user = { username: 'admin', password: 'admin' };
+const chests = require('./data/chests.json');
+const user = { username: process.env.ADMIN_USERNAME || 'admin', password: process.env.ADMIN_PASSWORD || 'admin' };
 const uri = 'http://localhost:8085/';
 
 request.post({
@@ -15,22 +16,22 @@ request.post({
   }, (err, httpResponse, body) => {
     const token = body;
     save(token, 'api/cards', cards);
-    //save(token, 'api/chests', chests);
+    save(token, 'api/chests', chests);
     save(token, 'api/players', players);
     save(token, 'api/arenas', arenas);
-});
+  });
 
 const save = (token, endpoint, objects) => {
-  setInterval(objects.forEach((o) => {
+  objects.forEach((o) => {
     request.post({
-        url: uri + endpoint,
-        headers: {
-          'Authorization': token,
-          'Accept': 'application/json'
-        },
-        form: o
-      }, (err, httpResponse, body) => {
-        console.log(o);
-      });
-  }), 1000);
-};
+      url: uri + endpoint,
+      headers: {
+        'Authorization': token,
+        'Accept': 'application/json'
+      },
+      form: o
+    }, (err, httpResponse, body) => {
+      console.log(o);
+    });
+  });
+}

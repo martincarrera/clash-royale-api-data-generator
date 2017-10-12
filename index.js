ba8e8ff1456432520ed2299f0c43fc5f98a67625
Martin Carrera <mcarrera.92@gmail.com>
2017-10-12 16:16:16 -0300

    Fix order

--- index.js
+++ index.js
@@ -1,39 +1,48 @@
-require('dotenv').load();
-const request = require('request');
-const arenas = require('./data/arenas.json');
-const cards = require('./data/cards.json');
-const players = require('./data/players.json');
-const chests = require('./data/chests.json');
-const leagues = require('./data/leagues.json');
-const user = { username: process.env.ADMIN_USERNAME || 'admin', password: process.env.ADMIN_PASSWORD || 'admin' };
-const uri = 'http://localhost:8085/';
+require("dotenv").load();
+const request = require("request");
+const arenas = require("./data/arenas.json");
+const cards = require("./data/cards.json");
+const players = require("./data/players.json");
+const chests = require("./data/chests.json");
+const leagues = require("./data/leagues.json");
+const user = {
+  username: process.env.ADMIN_USERNAME || "admin",
+  password: process.env.ADMIN_PASSWORD || "admin"
+};
+const uri = "http://localhost:8085/";
 
-request.post({
-    url: uri + 'api/authenticate',
+request.post(
+  {
+    url: uri + "api/authenticate",
     headers: {
-      'Accept': 'application/json'
+      Accept: "application/json"
     },
     form: user
-  }, (err, httpResponse, body) => {
+  },
+  async (err, httpResponse, body) => {
     const token = body;
-    // save(token, 'api/cards', cards);
-    // save(token, 'api/chests', chests);
-    // save(token, 'api/players', players);
-    // save(token, 'api/leagues', leagues);
-    save(token, 'api/arenas', arenas);
-  });
+    // await save(token, "api/cards", cards);
+    // await save(token, "api/chests", chests);
+    // await save(token, "api/players", players);
+    // await save(token, "api/leagues", leagues);
+    await save(token, "api/arenas", arenas);
+  }
+);
 
 const save = (token, endpoint, objects) => {
-  objects.forEach((o) => {
-    request.post({
-      url: uri + endpoint,
-      headers: {
-        'Authorization': token,
-        'Accept': 'application/json'
+  objects.forEach(o => {
+    request.post(
+      {
+        url: uri + endpoint,
+        headers: {
+          Authorization: token,
+          Accept: "application/json"
+        },
+        form: o
       },
-      form: o
-    }, (err, httpResponse, body) => {
-      console.log(o);
-    });
+      (err, httpResponse, body) => {
+        console.log(o);
+      }
+    );
   });
-}
+};
